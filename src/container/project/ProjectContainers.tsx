7a7d563f683022c493013f632921460c7e123273
Asd77811 <yout20030@gmail.com>
2024-10-10 12:31:58 +0530

    boiler plate code added

---
+++ src/container/project/ProjectContainers.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProjectContainers = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProjectContainers
---
+++ src/utils/constants.ts
@@ -0,0 +1,30 @@
+import React from "react"
+
+
+
+
+
+export const AppRoutes:IAppRoutes ={
+    {
+        __id='1',
+        name='Home',
+        URL:'/Home',
+        element:<Homepage/>
+    }
+
+    {
+        __id='2',
+        name='About',
+        URL:'/About',
+    }
+    {
+        __id='3',
+        name='Contact',
+        URL:'/Contact',
+    }
+    {
+        __id='4',
+        name='Project'
+        URL:'/project',
+    }
+}
